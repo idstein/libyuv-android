01362ca5ff065a896208100b19040ac8ffafd333
Idstein, Paul <pidstein@agtinternational.com>
2018-12-06 12:55:44 +0100

    Update to latest libyuv

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,6 +8,11 @@ LOCAL_CPP_EXTENSION := .cc
 LOCAL_SRC_FILES := \
     source/compare.cc           \
     source/compare_common.cc    \
+    source/compare_gcc.cc       \
+    source/compare_mmi.cc       \
+    source/compare_msa.cc       \
+    source/compare_neon.cc      \
+    source/compare_neon64.cc    \
     source/convert.cc           \
     source/convert_argb.cc      \
     source/convert_from.cc      \
@@ -20,47 +25,44 @@ LOCAL_SRC_FILES := \
     source/rotate_any.cc        \
     source/rotate_argb.cc       \
     source/rotate_common.cc     \
+    source/rotate_gcc.cc        \
+    source/rotate_mmi.cc        \
+    source/rotate_msa.cc        \
+    source/rotate_neon.cc       \
+    source/rotate_neon64.cc     \
     source/row_any.cc           \
     source/row_common.cc        \
+    source/row_gcc.cc           \
+    source/row_mmi.cc           \
+    source/row_msa.cc           \
+    source/row_neon.cc          \
+    source/row_neon64.cc        \
     source/scale.cc             \
     source/scale_any.cc         \
     source/scale_argb.cc        \
     source/scale_common.cc      \
+    source/scale_gcc.cc         \
+    source/scale_mmi.cc         \
+    source/scale_msa.cc         \
+    source/scale_neon.cc        \
+    source/scale_neon64.cc      \
     source/video_common.cc
 
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-    LOCAL_CFLAGS += -DLIBYUV_NEON
-    LOCAL_SRC_FILES += \
-        source/compare_neon.cc.neon    \
-        source/rotate_neon.cc.neon     \
-        source/row_neon.cc.neon        \
-        source/scale_neon.cc.neon
-endif
+common_CFLAGS := -Wall -fexceptions
 
-ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-    LOCAL_CFLAGS += -DLIBYUV_NEON
-    LOCAL_SRC_FILES += \
-        source/compare_neon64.cc    \
-        source/rotate_neon64.cc     \
-        source/row_neon64.cc        \
-        source/scale_neon64.cc 
-endif
-
-ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI), x86 x86_64))
-    LOCAL_SRC_FILES += \
-        source/compare_gcc.cc       \
-        source/rotate_gcc.cc        \
-        source/row_gcc.cc           \
-        source/scale_gcc.cc
-endif
-
-
+LOCAL_CFLAGS += $(common_CFLAGS)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 
-LOCAL_MODULE := libyuv
+LOCAL_MODULE := libyuv_static
 LOCAL_MODULE_TAGS := optional
 
-include $(BUILD_SHARED_LIBRARY)
-#include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
+include $(CLEAR_VARS)
+
+LOCAL_WHOLE_STATIC_LIBRARIES := libyuv_static
+LOCAL_MODULE := libyuv
+
+include $(BUILD_SHARED_LIBRARY)
